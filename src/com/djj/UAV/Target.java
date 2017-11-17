653c9791206d0a9c94f50c821a0796bedaaf4cad
Demo-du <1556141770@qq.com>
2017-11-17 09:56:09 +0800

    目标建模

---
+++ src/com/djj/UAV/Target.java
@@ -0,0 +1,15 @@
+package com.djj.UAV;
+
+public class Target {
+     public int x;
+     public int y;
+     int node;
+     public Target(){
+    	 
+     }
+     public Target(int node,int x,int y){
+    	 this.x=x;
+    	 this.y=y;
+    	 this.node=node;
+     }
+}
